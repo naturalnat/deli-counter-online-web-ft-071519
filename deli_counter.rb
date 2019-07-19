c7ae6fa0be73c68caff57fab3aa965bb30cb2ad3
Natalie Taktachev <ntaktach@gmail.com>
2019-07-19 16:52:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def line(array)
+
+  if array.length == 0
+    puts "The line is currently empty."
+
+  else
+      current_line = "The line is currently"
+      array.each_with_index do |person, index|
+      indexplusone = index + 1
+      current_line = "The line is currently" << "#{indexplusone}. #{person}"
+    end
+    puts current_line
+  end
+end
+
+def take_a_number(line, customer)
+  line = []
+  newline = line << customer
+
+  newline.each_with_index do |person, index|
+    indexplusone = index + 1
+    puts "Welcome, #{person}. You are number #{indexplusone} in line."
+  end
+end
